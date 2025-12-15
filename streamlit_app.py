4a41a1bc6d9d6d49004497a64043f4cfabd32a65
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-12-15 16:13:05 +0530

    Update streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from core.run_agent import app
+from langchain_core.messages import HumanMessage, AIMessage
+
+st.set_page_config(page_title="RAG Chatbot",page_icon = "🌱")
+st.title("🌱 Plant RAG Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+
+user_input = st.text_input("Ask me anything about plants: ")
+
+if user_input:
+    st.session_state.messages.append(HumanMessage(content=user_input))
+
+    state = {
+        "question": user_input,
+        "messages": st.session_state.messages,
+        "route": None,
+        "retrieved_docs": [],
+        "web_retrievals": [],
+        "enough_info": None,
+        "final_answer": None
+    }
+
+    result = app.invoke(state)
+
+    final_answer = result.get("final_answer","No answer generated")
+
+    st.session_state.messages.append(AIMessage(content=final_answer))
+
+for msg in st.session_state.messages:
+    if isinstance(msg, HumanMessage):
+        st.markdown(f"**🧑 You:** {msg.content}")
+
+    else:
+        st.markdown(f"**🤖 Bot:** {msg.content}")
