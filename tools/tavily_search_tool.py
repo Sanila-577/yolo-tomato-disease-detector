f9a220243f5d9b4ae4a96dcf6f7cda06aa05725c
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-12-15 12:27:19 +0530

    added state and llm

--- tools/tavily_search_tool.py
+++ tools/tavily_search_tool.py
@@ -1,3 +1,18 @@
+from typing import TypedDict, Annotated, Sequence, Optional, List, Literal
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from tavily import TavilyClient
+
+load_dotenv()
+
 @tool
 def tavily_search_tool(query: str) -> str:
     """
