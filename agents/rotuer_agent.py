98c6b61869098e8435e5776a8a49d1f58f3252c1
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-12-15 12:41:05 +0530

    added the necessary codes for agents

--- agents/rotuer_agent.py
+++ agents/rotuer_agent.py
@@ -12,3 +12,30 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from tavily import TavilyClient
 
 load_dotenv()
+
+def router_agent(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(
+        content = """
+        Classify the user's question into one of these intents:
+        - chat (greetings or casual conversation)
+        - rag (plant knowledge)
+        - web (general knowledge)
+
+        Answer with only one intent: chat, rag, or web.
+        Do Not Include Any Other Text
+        """
+    )
+
+    messages = [system_prompt] + state["messages"]
+    response = llm.invoke(messages)
+
+    route = response.content.strip().lower().replace("\n", "").replace(" ", "")
+
+    # Only allow valid routes
+    if route not in ["chat", "rag", "web"]:
+        print(f"⚠️ Router returned invalid route: {repr(route)}. Defaulting to 'chat'.")
+        route = "chat"
+
+    state["route"] = route
+    print(f"🔀 Router: Routed to {state['route']}")
+    return state
--- core/build_graph.py
+++ core/build_graph.py
@@ -1,14 +1,51 @@
-from typing import TypedDict, Annotated, Sequence, Optional, List, Literal
-from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage, HumanMessage
-from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from dotenv import load_dotenv
-import os
-from langchain_core.tools import tool
-from langgraph.prebuilt import ToolNode
-
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from tavily import TavilyClient
+from langgraph.graph import StateGraph, END
+from agents.state import AgentState
+from agents.router_agent import router_agent
+from agents.chat_agent import chat_agent
+from agents.retriever_agent import retrieve_agent
+from agents.web_agent import web_answer_agent
+from agents.grader_agent import unified_grader_answer_agent
 
 load_dotenv()
+
+def build_graph():
+    graph = StateGraph(AgentState)
+
+    graph.add_node("router", router_agent)
+    graph.add_node("chat_agent", chat_agent)
+    graph.add_node("retriever", retrieve_agent)
+    graph.add_node("web_scraper", web_answer_agent)
+    graph.add_node("grader_answer_generator", unified_grader_answer_agent)
+
+    graph.set_entry_point("router")
+
+    # conditonal edges
+    graph.add_conditional_edges(
+        "router",
+        lambda s: s["route"],
+        {
+            "chat":"chat_agent",
+            "rag": "retriever",
+            "web": "web_scraper"
+        }
+    )
+
+    graph.add_edge("retriever", "grader_answer_generator")
+    graph.add_edge("web_scraper", "grader_answer_generator")
+
+    graph.add_conditional_edges(
+        "grader_answer_generator",
+        lambda s: f"{s['route']}_{s['enough_info']}",  # string key
+        {
+            "rag_False": "web_scraper",
+            "rag_True": END,
+            "web_False": END,
+            "web_True": END
+        }
+    )
+
+    graph.add_edge("chat_agent", END)
+
+    app = graph.compile()
+
+    return app
