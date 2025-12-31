3a415a80e0da20c4bbac3272b6602651f4744b0b
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-12-31 17:41:56 +0530

    Added routes in main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from api.routes.health import router as health_router
+from api.routes.chat import router as chat_router
+from api.routes.detect import router as detect_router
 
 app = FastAPI(
     title="Tomato plant disease detection",
@@ -11,3 +13,6 @@ async def root():
     return {"messege": "Hello World"}
 
 app.include_router(health_router) # Register the router
+app.include_router(detect_router)
+app.include_router(chat_router)
+
