b72ac549b46e7418cb58b0a46d5f053a765e8786
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-12-31 23:19:19 +0530

    Implemented detect endpoint

--- vision/inference.py
+++ vision/inference.py
@@ -1,41 +1,43 @@
 import cv2
-from typing import Tuple, List
 from vision.model import yolo_model
 from vision.utils import draw_boxes, save_output_image
+import numpy as np
 
-def run_yolo_inference(image_path: str):
-    image = cv2.imread(image_path)
+def run_yolo_inference(image_bytes: bytes):
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    if image is None:
+        raise ValueError("Failed to decode image bytes")
+
     results = yolo_model(image)
+    result = results[0]
 
     detections = []
-    disease_labels = set()
+    disease_labels = []
 
-    for box in results.boxes:
-        cls_id = int(box.cls[0])
-        label = yolo_model.names[cls_id]
-        confidence = float(box.conf[0])
+    if result.boxes is not None:
+        for box in result.boxes:
+            cls_id = int(box.cls[0])
+            label = yolo_model.names[cls_id]
+            confidence = float(box.conf[0])
 
-        x1, y1, x2, y2 = box.xyxy[0].tolist()
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
 
-        detections.append({
-            "x1": x1,
-            "y1": y1,
-            "x2": x2,
-            "y2": y2,
-            "confidence": confidence,
-            "label": label
+            detections.append({
+                "x1": x1,
+                "y1": y1,
+                "x2": x2,
+                "y2": y2,
+                "confidence": confidence,
+                "label": label
+            })
 
-        })
+            disease_labels.append(label)
 
-        disease_labels.add(label)
-
-    # Pick most frequent disease
     detected_disease = (
-        max(disease_labels, key=lambda x: sum(d["label"] == x for d in detections))
+        max(set(disease_labels), key=disease_labels.count)
         if disease_labels else "Healthy"
-
     )
 
-    image_with_boxes = draw_boxes(image, detections)
-    output_path = save_output_image(image_with_boxes, "detected.jpg")
-    return detected_disease, detections, output_path
+    return detected_disease, detections, image
