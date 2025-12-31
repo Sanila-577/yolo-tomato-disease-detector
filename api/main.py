e8b3324ca021c84df7e72cdd93bd45c674611d96
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-12-31 17:29:48 +0530

    Made the api endpoints

--- api/main.py
+++ api/main.py
@@ -1,2 +1,13 @@
 from fastapi import FastAPI
+from api.routes.health import router as health_router
 
+app = FastAPI(
+    title="Tomato plant disease detection",
+    version="1.0.0"
+)
+
+@app.get("/")
+async def root():
+    return {"messege": "Hello World"}
+
+app.include_router(health_router) # Register the router
