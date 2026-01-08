638bc65c4bf365d86aa929608167ef6838963ec3
Sanila Wijesekara <sanilamethwan@gmail.com>
2026-01-08 20:36:58 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from api.routes.health import router as health_router
 from api.routes.chat import router as chat_router
 from api.routes.detect import router as detect_router
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Tomato plant disease detection",
@@ -17,4 +18,11 @@ app.include_router(health_router) # Register the router
 app.include_router(detect_router)
 app.include_router(chat_router)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows Streamlit Cloud to connect
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
